d4cba498e6b8851deb5e992792320baf4df2a2bc
Ezra Vance <3zravance@gmail.com>
2020-07-10 00:49:56 -0400

    linking up the js file to the web pages.

--- js/events.js
+++ js/events.js
@@ -1,12 +1,14 @@
 var bootButton = document.getElementById("boot");
 bootButton.addEventListener('click', playBoot(document.title));
 
+console.log(document.title); //test to see what title value is
+
 function playBoot(page){
     if (page === "Dreamcast Page"){
 
     }else if (page === "Playstation 2 Page"){
 
     }else{
-        
+
     }
 }
