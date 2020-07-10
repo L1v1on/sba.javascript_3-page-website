b9d21de21324e3b31aa66816a284f306194776c8
Ezra Vance <3zravance@gmail.com>
2020-07-10 10:51:47 -0400

    created my quiz function and figured out getElementById().

--- js/quiz.js
+++ js/quiz.js
@@ -1,1 +1,32 @@
-// Remake this into a prompt quiz thingy
+
+var question = ["What year did the dream die?", "What year did the behemonth appear?",
+"Is the gamecube cool?"];
+var ans;
+var loop = true;
+
+function myQuestion(){
+    var pageOn = document.getElementById("title");
+    console.log(pageOn.textContent);
+
+    do{
+        if(pageOn.textContent === "Dreamcast Page"){
+            ans = prompt(question[0]);
+            console.log(ans);
+            if(ans === "2001"){
+                loop = false;
+            }
+        }else if(pageOn === "Playstation 2 Page"){
+            ans = prompt(question[1]);
+            if(ans === "2000"){
+                loop = false;
+            }
+        }else{
+            ans = prompt(question[2]);
+            if(ans.toLocaleLowerCase() === "yes"){
+                loop = false;
+            }
+        }
+        console.log(ans);
+        console.log(loop);
+    }while(loop);
+}
