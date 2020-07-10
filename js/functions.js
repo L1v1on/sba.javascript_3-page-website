e269ea74a0fca135f8e40be84e101677f47c604a
Ezra Vance <3zravance@gmail.com>
2020-07-10 02:08:47 -0400

    fighting with getting audio to play on button click.

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,8 @@
-var audio = document.getElementById("bootAudio");
-console.log(audio); //test to see what audio value is
-
 function playBoot(page){
     if (page === "Dreamcast Page"){
+        var audio = document.createElement('audio');
         
+        //audio.play();
     }else if (page === "Playstation 2 Page"){
 
     }else{
