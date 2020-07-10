b3a74453ffb798a9104df09ddd412b045b67f75f
Ezra Vance <3zravance@gmail.com>
2020-07-10 00:42:08 -0400

    setup for button boot event.

--- js/events.js
+++ js/events.js
@@ -1,3 +1,12 @@
-function playBoot(button){
-    var button
+var bootButton = document.getElementById("boot");
+bootButton.addEventListener('click', playBoot(document.title));
+
+function playBoot(page){
+    if (page === "Dreamcast Page"){
+
+    }else if (page === "Playstation 2 Page"){
+
+    }else{
+        
+    }
 }
