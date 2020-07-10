72a96d385e699d97d4ebcf38cf3a4cc4c009ff4d
Ezra Vance <3zravance@gmail.com>
2020-07-10 02:24:12 -0400

    finally got the boot sound button working on all web pages.

--- js/events.js
+++ js/events.js
@@ -1,12 +1,5 @@
 var audio = document.getElementById("bootAudio");
-console.log(audio); //test to see what audio value is
 
-function playBoot(page){
-    if (page === "Dreamcast Page"){
-        
-    }else if (page === "Playstation 2 Page"){
-
-    }else{
-
-    }
+function playBoot(){
+    audio.play();
 }
